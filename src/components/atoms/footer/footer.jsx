44b10a18261a6ba1c96c354ba2d225ddac4402de
Muhammad Anies Wahdie <manies5396@gmail.com>
2025-09-23 15:52:46 +0700

    fix: UI login form done

---
+++ src/components/atoms/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 py-4">
+      <p className=" text-[#232b2b] text-sm opacity-50 font-semibold text-center">
+        &copy; 2025 Sim Clinic Powered By Ara Medika | All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/atoms/header/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="flex flex-row justify-between items-center px-5 min-h-[56px] bg-white fixed top-0 left-0 right-0 z-10">
+      <img src="../../../public/ara-medika.svg" width={50} height={50} />
+    </div>
+  );
+};
+
+export default Header;
