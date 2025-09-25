3fd1eb778a0d96864cfe26c22e7fb8e80aaade7f
Muhammad Anies Wahdie <manies5396@gmail.com>
2025-09-25 16:12:25 +0700

    fix: add logged name

--- src/components/atoms/header/header.jsx
+++ src/components/atoms/header/header.jsx
@@ -8,19 +8,25 @@ import DrawerMenu from "../../molecules/drawers/menu-drawer";
 const Header = () => {
   const setDrawermenuOpen = useMenuDrawer((state) => state.setDrawermenuOpen);
   const isAuthorize = useAuth((state) => state.isAuthorize);
+  const username = useAuth((state) => state.username);
 
   return (
     <div className="flex flex-row justify-between items-center px-5 min-h-[56px] bg-white fixed top-0 left-0 right-0 z-10">
       <img src={LogoAraMedika} width={50} height={50} />
       {isAuthorize && (
-        <FontAwesomeIcon
-          icon={faBars}
-          width={50}
-          height={50}
-          className="cursor-pointer"
-          color="#2e5b36"
-          onClick={setDrawermenuOpen}
-        />
+        <div className="flex flex-row gap-2 items-center">
+          <span className="text-sm font-semibold opacity-70 text-primary-green">
+            {username}
+          </span>
+          <FontAwesomeIcon
+            icon={faBars}
+            width={50}
+            height={50}
+            className="cursor-pointer"
+            color="#2e5b36"
+            onClick={setDrawermenuOpen}
+          />
+        </div>
       )}
       <DrawerMenu />
     </div>
--- src/pages/welcome-pages/index.jsx
+++ src/pages/welcome-pages/index.jsx
@@ -1,12 +1,13 @@
-import React from "react";
 import DefaultLayout from "../../components/organism/layouts";
+import useAuth from "../../stores/useAuth";
 
 const WelcomePages = () => {
+  const username = useAuth((state) => state.username);
   return (
     <>
       <DefaultLayout>
         <div className="min-h-[75vh] min-w-[95vw] max-w-[100vw] rounded-2xl p-2 bg-white">
-          Selamat Datang, Muhammad Anies Wahdie!
+          Selamat Datang, {username}!
         </div>
       </DefaultLayout>
     </>
